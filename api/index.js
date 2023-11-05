51230fa2e7b8e64311e466d6f3f689bae3362c3c
Harshika Govind <harshika0102@gmail.com>
2023-11-05 13:02:54 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 
 mongoose
@@ -13,7 +14,9 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running in 3000 port!!!");
 });
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
